0792e9da3400d0611b739731454f5ec4edb5c062
Hamzah Mahomed <hamzah.m@sedgent.com>
2025-10-23 15:56:16 +0000

    i wanna have this page hosted as a static page on github pages. Please c

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,18 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  devIndicators: {
+    buildActivity: false,
+  },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
